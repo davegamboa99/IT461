0e15d87de2732b1b60040f2956ab9931f5faf3f1
DaveG <53077307+davegamboa99@users.noreply.github.com>
2022-04-29 12:03:36 +0800

    cats, cats/<id> endpoint

---
+++ README.md
@@ -0,0 +1,5 @@
+# IT461
+
+IT461 - Assignment
+
+AUTHOR: Gilmar Dave Gamboa
--- server/src/models/cat.py
+++ server/src/models/cat.py
@@ -1,8 +1,7 @@
 from flask import g
 from db import Db
 
-
-class Cat:
+class Cat():
     def sanitize(self, cats):
         if not isinstance(cats, (list, tuple)):
             cats = [cats]
@@ -10,7 +9,7 @@ class Cat:
         for cat in cats:
             if not isinstance(cat, dict):
                 continue
-            if not ("id" in cat and "name" in cat):
+            if not ('id' in cat and 'name' in cat):
                 continue
             clean_cats.append(cat)
         return clean_cats
@@ -24,7 +23,7 @@ class Cat:
         queries = []
         for cat in clean_cats:
             sql = "INSERT INTO cats(name) VALUES(%s)"
-            queries.append({"sql": sql, "bind": cat["name"]})
+            queries.append({"sql": sql, "bind": cat['name']})
         db = Db.get_instance()
         result = db.transactional(queries)
         return cats
@@ -32,9 +31,9 @@ class Cat:
     def get(self, filters=None):
         db = Db.get_instance()
         if filters is not None:
-            if "id" in filters:
+            if 'id' in filters:
                 sql = "SELECT * FROM cats WHERE id = %s"
-                cat = db.fetchone(sql, filters["id"])
+                cat = db.fetchone(sql, filters['id'])
                 return cat
             # if another filter
         sql = "SELECT * FROM cats ORDER BY name"
@@ -50,7 +49,7 @@ class Cat:
         queries = []
         for cat in clean_cats:
             sql = "UPDATE cats SET name = %s WHERE id = %s"
-            queries.append({"sql": sql, "bind": (cat["name"], cat["id"])})
+            queries.append({"sql": sql, "bind": (cat['name'], cat['id'])})
         db = Db.get_instance()
         db.transactional(queries)
         return cats
@@ -62,7 +61,7 @@ class Cat:
         placeholder = []
         queries = []
         for cat in cats:
-            placeholder.append("%s")
+            placeholder.append('%s')
         sql = "DELETE FROM cats WHERE id IN (" + ", ".join(placeholder) + ")"
         queries.append({"sql": sql, "bind": cats})
         db = Db.get_instance()
